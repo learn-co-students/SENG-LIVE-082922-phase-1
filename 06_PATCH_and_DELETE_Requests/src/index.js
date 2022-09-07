55730c1e7f82b3b2dd83c3345a403438733113ed
Louis <Louis.Medina@flatironschool.com>
2022-09-07 14:31:20 -0500

    add notes for lecture 06

--- 06_PATCH_and_DELETE_Requests/src/index.js
+++ 06_PATCH_and_DELETE_Requests/src/index.js
@@ -1,3 +1,8 @@
+// C => Create (POST)
+// R = Read (GET)
+// U => Update (PATCH / PUT)
+// D => Delete (DELETE)
+
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch requests 
         // Function for making a GET request 
@@ -6,15 +11,57 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(res => res.json())
         }
 
-        function createResources(url, body){
+        // /books + /stores => Adding New Individual Records
+        function createResource(url, body){
             return fetch(url,{
                 method: 'POST', 
                 headers: {
                   'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(body),
+                body: JSON.stringify(body)
             })
             .then(res => res.json())
+        }
+
+        // /books/:id => Update Individual Records
+        function updateResource(url, body) {
+            return fetch(url, {
+                method: 'PATCH', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body)
+            })
+
+            // res.json() => Parsing JSON to JS
+            // JSON.stringify(obj) => Parsing JS to JSON
+
+            .then(res => res.json())
+        }
+
+        function deleteResource(id, li) {
+            return fetch(`http://localhost:3000/books/${id}`, {
+                method: 'DELETE', 
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            .then(() => li.remove());
+        }
+
+        // function handleDelete(id) {
+        //     deleteResource(id)
+        //     .then(data => console.log(data))
+        //     .catch(error => console.error(error))
+        // }
+
+        function handleUpdate(newVal, id) {
+            updateResource(
+                `http://localhost:3000/books/${id}`, 
+                { inventory: newVal }
+            )
+            .then(book => console.log(book))
+            .catch(e => console.error(e))
         }
 
     // Rendering functions
@@ -37,20 +84,43 @@ document.addEventListener('DOMContentLoaded', () => {
             const pPrice = document.createElement('p')
             const img = document.createElement('img')
             const btn = document.createElement('button')
+            const updateBtn = document.createElement('button')
+            const pInventory = document.createElement('input')
     
             h3.textContent = cardData.title
             pAuthor.textContent = cardData.author
             pPrice.textContent = `$${cardData.price}`
             btn.textContent = 'Delete'
+            updateBtn.textContent = 'Update'
+            pInventory.value = cardData.inventory
     
             img.src = cardData.imageUrl
             li.className = 'list-li'
+            pInventory.type = 'number'
+            pInventory.id = `inventory-${cardData.id}`
     
             //Event Listeners 
-            btn.addEventListener('click',()=>li.remove())
+            btn.addEventListener('click', () =>  deleteResource(cardData.id, li))
         
-            li.append(h3,pAuthor,pPrice,img,btn)
+            li.append(h3,pAuthor,pPrice,pInventory,img,btn,updateBtn)
             document.querySelector('#book-list').append(li)
+
+            // console.log(cardData.id)
+            
+            // e.target.value => inventory input value
+            // cardData.id => unique id of book card in question
+            // handleUpdate(newVal, id)
+            
+            
+            // Tie UPDATE Fetch Requests to 'click' Event
+            updateBtn.addEventListener('click', () => { 
+                const newInventory = document.querySelector(`#inventory-${cardData.id}`).value
+                
+                handleUpdate(newInventory, cardData.id)
+            });
+
+            // Tie UPDATE Fetch Requests to 'change' Event
+            // pInventory.addEventListener('change', e => handleUpdate(e.target.value, cardData.id))
         }
     
     // Event Handlers
@@ -65,7 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 inventory:e.target.inventory.value,
                 reviews:[]
             }
-            createResources('http://localhost:3000/books', book)
+            createResource('http://localhost:3000/books', book)
             .then(renderBookCard)
             .catch(e => console.error(e))
 
@@ -85,5 +155,4 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(e => console.error(e))
     
         document.querySelector('#book-form').addEventListener('submit', handleForm)
-    
 })
